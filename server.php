fc3b63a9502ac30c4a8038a37c429c934f87de0b
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2022-07-01 08:59:29 -0400

    Curriculum is separate, not a subproject.

---
+++ server.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Server</title>
+  </head>
+  </body>
+    <h2>Environment</h2>
+    <pre>
+<?php
+  var_dump ($_ENV);
+ ?>
+    </pre>
+    <h2>Server</h2>
+    <pre>
+<?php
+  var_dump ($_SERVER);
+ ?>
+    </pre>
+  </body>
+</html>
+
